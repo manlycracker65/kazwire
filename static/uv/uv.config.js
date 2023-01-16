48f5c878553269b4a5a079ce2d87958394a6a0e0
manlycracker65 <103338779+manlycracker65@users.noreply.github.com>
2023-01-16 12:28:52 -0600

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,11 +1,19 @@
 /*global Ultraviolet*/
-self.__uv$config = {
-	prefix: '/search/',
-	bare: '/bare/',
-	encodeUrl: Ultraviolet.codec.xor.encode,
-	decodeUrl: Ultraviolet.codec.xor.decode,
-	handler: '/uv/uv.handler.js',
-	bundle: '/uv/uv.bundle.js',
-	config: '/uv/uv.config.js',
-	sw: '/uv/uv.sw.js'
-};
+{
+	"versions": [
+		"v1",
+		"v2"
+	],
+	"language": "NodeJS",
+	"memoryUsage": 599.12,
+	"maintainer": {
+		"email": "tomphttp@sys32.dev",
+		"website": "https://github.com/tomphttp/"
+	},
+	"project": {
+		"name": "bare-server-node",
+		"description": "TOMPHTTP NodeJS Bare Server",
+		"repository": "https://github.com/tomphttp/bare-server-node",
+		"version": "1.2.2"
+	}
+}
